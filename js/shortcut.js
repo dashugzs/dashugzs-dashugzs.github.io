742861e7dcff6f00ffa70bd8cdeb6a4977171235
dashugzs <neisidashu@126.com>
2022-10-21 11:39:46 +0800

    全新更新

---
+++ js/shortcut.js
@@ -0,0 +1,9 @@
+//id,iconName,iconUrl,url,width,height
+var shortcut = [
+[0,"小牛书签","https://img.alicdn.com/imgextra/i3/2327995847/O1CN01fhXl2q1t3yYaZYRnO_!!2327995847.png","https://b.xiaoniuss.top/",1536,756],
+[1,"小牛游戏","https://img.alicdn.com/imgextra/i3/2327995847/O1CN01fhXl2q1t3yYaZYRnO_!!2327995847.png","https://y.xiaoniuss.top/",1536,756],
+[2,"小牛插件","https://img.alicdn.com/imgextra/i3/2327995847/O1CN01fhXl2q1t3yYaZYRnO_!!2327995847.png","https://c.xiaoniuss.top/",1536,756],
+[3,"网址发布","https://img.alicdn.com/imgextra/i3/2327995847/O1CN01fhXl2q1t3yYaZYRnO_!!2327995847.png","https://b.xiaoniuss.top/zhuye",1536,756],
+[4,"vip视频解析","https://img.alicdn.com/imgextra/i3/2327995847/O1CN01fhXl2q1t3yYaZYRnO_!!2327995847.png","https://v.xiaoniuss.top/",1536,756],
+[5,"vip视频解析","https://img.alicdn.com/imgextra/i3/2327995847/O1CN01fhXl2q1t3yYaZYRnO_!!2327995847.png","https://v.xiaoniuss.top/",1536,756],
+];
